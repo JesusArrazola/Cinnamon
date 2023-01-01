48ca71b61a679ca15f66bbb605e576488ea04f1f
JesusArrazola <arrazola69@hotmail.com>
2023-01-01 14:30:13 -0600

    First Project Structure

---
+++ templates/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | My First blog</title>
+    <link rel="stylesheet" href="../styles/home.css">
+</head>
+<body>
+    <header>
+        <p class="logo global-margin-left">My first blog</p>
+        <nav class="global-margin-right" class="cn_menu-holder">
+            <a href="#">Inicio</a>
+            <a href="#">Blog</a>
+            <a href="#">Acerca de</a>
+        </nav>
+    </header>
+    <article class="global-margin">
+        <h1>Welcome to my first blog! Powered by Cinnamon</h1>
+    </article>
+</body>
+</html>
